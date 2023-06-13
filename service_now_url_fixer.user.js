7df90a76c7ba209e0c704609f9128b66bb87aa95
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2023-06-13 11:36:45 -0400

    Create service_now_url_fixer.user.js

---
+++ service_now_url_fixer.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Service now fixer
+// @namespace   Violentmonkey Scripts
+// @match       [https://*.service-now.com/sc*.do]https://*.service-now.com/sc*.do
+// @grant       none
+// @version     1.0
+// @author      Andrew Friedman
+// @description 6/13/2023, 11:01:21 AM
+// @updateURL   https://github.com/arfrie22/fun-tampermonkey-scripts/raw/main/service_now_url_fixer.user.js
+// @downloadURL https://github.com/arfrie22/fun-tampermonkey-scripts/raw/main/service_now_url_fixer.user.js
+// ==/UserScript==
+
+let urlParts = window.location.href.split("/").slice();
+let encodedURL = encodeURIComponent(urlParts[urlParts.length - 1]);
+urlParts[urlParts.length - 1] = `now/nav/ui/classic/params/target/${encodedURL}`;
+window.location.href = urlParts.join("/");
